96804791bed3a1e72a520d901ded9181019179d4
Matheus <mdmaia91@gmail.com>
2022-08-18 18:19:29 -0300

    atualização

--- tasks/views.py
+++ tasks/views.py
@@ -1,17 +1,29 @@
-
 from django import forms
 from django.shortcuts import render
 tasks = ["foo", "bar", "baz"]
 
 class NewTaskform(forms.Form):
     task = forms.CharField(label= "New Task")
-
+    priority = forms.IntegerField(label="Priority", min_value=1, max_value=5)
+    
 def index (request):
     return render (request, "tasks/index.html",{
         "tasks":tasks
     })
 
 def add(request):
-    return render(request, "tasks/add.html",{
-        "form" : NewTaskform()
-    })
+        if request.method == "POST":
+            form = NewTaskform(request.POST)
+            if form.is_valid():
+                task = form.cleaned_data["task"]
+                task.append(task)
+            else:
+                return render (request, "tasks/add.html", {
+                    "form" : form
+                })
+                    
+
+        return render(request, "tasks/add.html", {
+            "form": NewTaskform()
+        })
+                 
