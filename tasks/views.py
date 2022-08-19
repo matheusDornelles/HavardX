b058b3b07e3ea56cdf96ccbab093dbdc448efd0c
Matheus <mdmaia91@gmail.com>
2022-08-19 16:25:43 -0300

    add button att

--- tasks/views.py
+++ tasks/views.py
@@ -1,10 +1,15 @@
+from audioop import reverse
+from http.client import HTTPResponse
+from django.http import HttpResponseRedirect
 from django import forms
 from django.shortcuts import render
-tasks = ["foo", "bar", "baz"]
+from django.urls import reverse 
+
+tasks = []
 
 class NewTaskform(forms.Form):
     task = forms.CharField(label= "New Task")
-    priority = forms.IntegerField(label="Priority", min_value=1, max_value=5)
+    
     
 def index (request):
     return render (request, "tasks/index.html",{
@@ -16,7 +21,8 @@ def add(request):
             form = NewTaskform(request.POST)
             if form.is_valid():
                 task = form.cleaned_data["task"]
-                task.append(task)
+                tasks.append(task)
+                return HttpResponseRedirect(reverse("tasks:index"))
             else:
                 return render (request, "tasks/add.html", {
                     "form" : form
